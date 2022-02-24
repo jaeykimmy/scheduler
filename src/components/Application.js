99c5e6d2de5e8ce8ba7ed81447d6dee254e71af4
Jaeyoung Kim <j.jaeykim@gmail.com>
2022-02-24 18:59:21 -0500

    managing states with objects

--- src/components/Application.js
+++ src/components/Application.js
@@ -44,8 +44,17 @@ const appointments = [
 ];
 
 export default function Application() {
-  const [days, setDays] = useState([]);
-  const [day, setDay] = useState("Monday")
+  const [state, setState] = useState({
+  day: "Monday",
+  days: [],
+  
+  appointments: {}
+  });
+  
+  const setDay = day => setState({ ...state, day });
+  const setDays = (days) => {
+    setState(prev => ({ ...prev, days }));
+}
   const appointmentList = appointments.map((appointment) =>
     <Appointment
       key={appointment.id}
@@ -54,7 +63,7 @@ export default function Application() {
   
   useEffect(() => {
     axios.get('/api/days').then(response => {
-      setDays([...response.data])
+      setDays(response.data)
       console.log(response.data)
     })
 }, [])
@@ -70,8 +79,8 @@ export default function Application() {
 <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu" >
           <DayList
-            days={days}
-            value={day}
+            days={state.days}
+            value={state.day}
             onChange={setDay}
           />
 </nav>
