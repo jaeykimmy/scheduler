71db83897c0433e8651b31e40d2aeb090e6e1191
Jaeyoung Kim <j.jaeykim@gmail.com>
2022-02-25 19:25:58 -0500

    added custom hooks for transition and back

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+  
+  function transition(newMode, replace = false) {
+    if (replace) {
+      setHistory(prev => {
+        let replaceHistory = prev.slice(0, -1)
+        return [...replaceHistory, newMode]
+      })
+    } else {
+      setHistory([...history, newMode])
+    }
+      setMode(newMode)
+  }
+  function back() {
+    if (history.length > 1) {
+      const newHistory = history.slice(0, -1)
+      setHistory(newHistory)
+      setMode(newHistory[newHistory.length-1])
+      
+    }
+  }
+
+  return { mode, transition, back };
+}
